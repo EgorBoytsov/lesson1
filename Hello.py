0e393eb283667fb69a089e35f482a2d7d45a1358
Бойцов Егор <jlemark@MacBook-Air-Bojcov.local>
2023-05-14 20:53:02 +0300

    Revert "First commit"
    This reverts commit c5c2ca112cd882e42a97ffcafa38a78f503a83f9.

--- Hello.py
+++
@@ -1,5 +0,0 @@
-name = "Egor"
-print(name)
-print('Привет программист!')
-print(2 + 2)
-print(10 / 3)
--- price.py
+++
@@ -1,6 +0,0 @@
-def format_price(price):
-    price = int(price)
-    print(f'Цена: {price} руб.')
-
-price = float(input("Введите число: "))
-print(format_price(price))
